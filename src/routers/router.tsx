3093f8c452123023258347f604a47678deac96fd
ivancidev <herlanherbas@gmail.com>
2024-09-28 21:17:22 -0400

    Refactor router configuration and layout structure

---
+++ src/routers/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Root from '../layouts/router-root';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />
+  },
+]);
