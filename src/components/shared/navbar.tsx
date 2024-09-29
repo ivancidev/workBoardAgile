92d0ac1ccc81c4cb46270b8b3817442a613c0964
ivancidev <herlanherbas@gmail.com>
2024-09-28 21:17:27 -0400

    Add navbar component with logo image

---
+++ src/components/shared/navbar.tsx
@@ -0,0 +1,11 @@
+import Logo from "../../assets/images/logo.png";
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <img src={Logo} alt="logo" className="w-28 rounded" />
+        
+      </div>
+    </div>
+  );
+}
