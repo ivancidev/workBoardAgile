584577e5076af63f3d4cab42feefac218e1d18de
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-01-19 19:43:09 -0400

    feat: add icon message

---
+++ src/components/shared/icons/message-icon.tsx
@@ -0,0 +1,18 @@
+export const MessageIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-message-square"
+    >
+      <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+    </svg>
+  );
+};
