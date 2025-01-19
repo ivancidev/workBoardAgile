dbd73679e97a2529c353104779022f1b787864ba
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-01-19 19:43:40 -0400

    feat: enhance CardTask component with footer and date formatting

--- src/features/tasks/components/card-task.tsx
+++ src/features/tasks/components/card-task.tsx
@@ -2,17 +2,20 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components';
+import { MessageIcon } from '@/components/shared/icons/message-icon';
 import { Task } from '@/types';
+import { convertDate } from '@/utils/convert-date';
 interface CardTaskProps {
   task: Task;
 }
 
 export const CardTask = ({ task }: CardTaskProps) => {
   return (
-    <Card className="w-full max-w-sm cursor-pointer">
+    <Card className="w-full max-w-sm cursor-pointer border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-all duration-300">
       <CardHeader>
         <CardTitle>{task.title}</CardTitle>
         <div className="flex gap-2">
@@ -25,11 +28,34 @@ export const CardTask = ({ task }: CardTaskProps) => {
             </span>
           ))}
         </div>
-        <CardDescription>{task.description}</CardDescription>
       </CardHeader>
       <CardContent>
-        <p>{task.date}</p>
+        <CardDescription>{task.description}</CardDescription>
       </CardContent>
+      <CardFooter className="flex justify-between items-center">
+        <p className="text-xs font-semibold">{convertDate(task.date)}</p>
+        <div className="flex items-center gap-4">
+          <span className='flex items-center gap-1'>
+            <MessageIcon />
+            <p>1</p>
+          </span>
+          <div className="flex items-center">
+            {task.users.slice(0, 2).map((user) => (
+              <img
+                key={user.id}
+                src={user.avatarUrl}
+                alt={user.name}
+                className="w-7 h-7 rounded-full border-2 border-white shadow-sm"
+              />
+            ))}
+            {task.users.length > 2 && (
+              <div className="w-7 h-7 rounded-full border-2 border-white shadow-sm flex items-center justify-center bg-gray-200 text-xs font-semibold">
+                +{task.users.length - 2}
+              </div>
+            )}
+          </div>
+        </div>
+      </CardFooter>
     </Card>
   );
 };
